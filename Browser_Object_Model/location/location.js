96180b5366fcc475af03bde5e18f6b78aa44979b
Soumik Ghosh <115470141+Soumik-Ghosh-2924@users.noreply.github.com>
2024-06-22 13:53:01 +0530

    Add files via upload

---
+++ Browser_Object_Model/location/location.js
@@ -0,0 +1,14 @@
+//location object and its properties withing the browser object model ;
+
+
+function locationProperties()
+{
+
+			console.log("href of location : " + location.href);
+			document.write("<br> href of location : " + location.href);
+			console.log("protocol used  : " + location.protcol);
+			document.write("<br> protocol used  : " + location.protcol);
+
+}
+
+locationProperties();
